df5f01f1e7fd6b357e58e3c2c942c15e26a6222f
Dan Rolfe <git@rolfe.email>
2020-05-23 18:24:48 +0100

    Add area-type dropdown, tidy a bit

--- app.py
+++ app.py
@@ -23,35 +23,41 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'textAlign
         style={'textAlign': 'center', 'color': colors['text']
         }
     ),
-    html.Div(children=[
-        html.Div(style={ 'border': 'solid 1px #aaaaaa',
-                         'padding': '1em',
-                         'margin': '1em' },
-                 children=[
-            dcc.Slider(id='t_infectious_slider',
-                       min=1, max=21, step=1,
-                       marks={1: '1 day', 7: '7 days', 14: '14 days', 21: '21 days'},
-                       value=7,
-            ),
-            html.Div(id='t_infectious_slider_value'),
-            ]),
-        html.Div(id='coronavirus_plot_div',
-                 children=[html.Img(id='coronavirus_plot_img', src='')]
-        )
-    ])
+    html.Label('Statistics for'),
+    dcc.Dropdown(id='area_type_dropdown',
+                 options=[{'label': t, 'value': t} for t in ['Nation', 'Region']],
+                 value='Nation'),
+    html.Div(style={'padding': '10px'}),
+    html.Label(id='t_infectious_slider_value'),
+    html.Div(style={ 'border': 'solid 1px #cccccc',
+                     'padding': '0.5em',
+                     'border-radius': '4px', 
+    },
+             children=[
+                 dcc.Slider(id='t_infectious_slider',
+                            min=1, max=21, step=1,
+                            marks={1: '1 day', 7: '7 days', 14: '14 days', 21: '21 days'},
+                            value=7,
+                 ),
+             ]),
+    html.Div(style={'padding': '10px'}),
+    html.Div(id='coronavirus_plot_div',
+             children=[html.Img(id='coronavirus_plot_img', src='')]
+    )
 ])
 
 @app.callback(
     [dash.dependencies.Output('coronavirus_plot_img', 'src'),
      dash.dependencies.Output('t_infectious_slider_value', 'children')],
-    [dash.dependencies.Input('t_infectious_slider', 'value')])
-def update_coronavirus_plot(t_inf):
+    [dash.dependencies.Input('t_infectious_slider', 'value'),
+     dash.dependencies.Input('area_type_dropdown', 'value'),
+    ])
+def update_coronavirus_plot(t_inf, area_type):
     # Convert MatPlotLib figure encoded as PNG image. More clunky than using
     # plotly Graphs but I want to see how well it works as I have many complex
     # maplotlib visualisations in existing projects which I want to use.
     fig=coronavirus.Plot(data,
-                         #area_type='Nation',
-                         area_type='Region',
+                         area_type=area_type,
                          t_infectious=t_inf # Days for which a patient is infectious
     )
     return utils.fig_to_uri(fig, tight_layout=True, dpi=64), "Assume people are infectious for {} days".format(t_inf)
