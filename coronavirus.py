56881aef7780dc86063fa3af26452a0dafdf262d
Dan Rolfe <git@rolfe.email>
2020-05-26 10:37:26 +0100

    Improve plot

--- coronavirus.py
+++ coronavirus.py
@@ -21,6 +21,8 @@ class Data:
 
     def listAreas(self, area_type):
         return set(self.area_names[np.where(self.area_types == area_type)])
+    def listAreaTypes(self):
+        return list(set(self.area_types))
     def getCurveForArea(self, area_name, smooth=False):
         warea=np.where(self.area_names == area_name)
         date=pd.to_datetime(self.data['Specimen date']).to_numpy()[warea]
@@ -52,25 +54,32 @@ def NInfectious(curve, t_infectious):
 def Plot(data, area_type, t_infectious, smooth):
     size_cm=(21.0, 29.7)
     fig=plt.figure(figsize=(size_cm[0]/2.54, size_cm[1]/2.54))
-    #fig=plt.figure()
     Dax=fig.add_subplot(2, 1, 1)
     Rax=fig.add_subplot(2, 1, 2)
-    Dax.set_xticks([])
     #Dax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
     #Dax.set_xlabel("Date")
+    #Dax.set_xticks([])
+    Dax.set_xticklabels([])
     Dax.set_ylabel("Daily cases")
-    #plt.setp(Dax.get_xticklabels(), rotation=45, ha='right')
+    plt.setp(Dax.get_xticklabels(), rotation=45, ha='right')
     Rax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
     Rax.set_xlabel("Date")
     Rax.set_ylabel("R value")
     plt.setp(Rax.get_xticklabels(), rotation=45, ha='right')
+    daterange=[np.nan, np.nan]
     for area in data.listAreas(area_type):
         curve=data.getCurveForArea(area, smooth=smooth)
         n_infectious=NInfectious(curve, t_infectious)
         R=curve['daily']*t_infectious/n_infectious
         Dax.plot(curve['datenum'], curve['daily'], label=area)
         Rax.plot(curve['datenum'], R, label=area)
+        daterange=[np.nanmin([daterange[0], curve['datenum'].min()]),
+                   np.nanmax([daterange[0], curve['datenum'].max()]),
+        ]
+    Rax.plot(daterange, [1, 1], ls=':', color='black')
+    Dax.set_xlim(daterange)
     Dax.legend()
+    Rax.set_xlim(daterange)
     Rax.legend()
     return fig
 
